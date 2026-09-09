c7ff4b012b96154a8b44cc6efe70d860f72c2789
nishant-veracode <nmishra@veracode.com>
2026-09-09 11:26:18 +0530

    Add runFixSast function for SAST processing
    Implement runFixSast function to execute Veracode CLI for SAST fixes.

---
+++ src/run-fix-sast.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+async function runFixSast(workspaceDir, actionPath, fixScaParams, sourceCodeDir) {
+  try {
+    // Set up environment for veracode CLI
+    const isWindows = process.platform === 'win32';
+    const binaryName = isWindows ? 'veracode.exe' : 'veracode';
+    const veracodeBinary = path.join(`${process.env.CLI_PATH}`, binaryName);
+
+    // Build command arguments
+    const args = [
+      'fix',
+      'sast',
+      sourceCodeDir,
+      '--results',
+      path.join(
+        workspaceDir,
+        'veracode_artifact_directory',
+        'scaResults.json'
+      ),
+      '--async',
+      '--decouple',
+      'true',
+    ];
+
+    core.info('--------- Running inside fix for sast ---------');
+    // Conditionally add --remote flag (default: false)
+    const fixRemote = core.getInput('fix-remote');
+    if (fixRemote?.toLowerCase() === 'true') {
+      core.info(`remote argument appended`)
+      args.push('--remote');
+    }
+
+    return { hasChanges: true };
+  } catch (error) {
+    throw new Error(`Failed to run Fix for SCA: ${error.message}`);
+  }
+}
+
+module.exports = runFixSca;
